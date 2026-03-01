cfd6e69a091298bad817d583c2209a9fffbb53da
Maxim <36982547+Siphoin@users.noreply.github.com>
2026-03-01 16:26:35 +0500

    add nickName View

---
+++ Assets/Scripts/UI/Configs/NickNameViewConfig.cs
@@ -0,0 +1,17 @@
+﻿using AvatarChat.Main.Configs;
+using UnityEngine;
+
+namespace AvatarChat.UI.Configs
+{
+    [CreateAssetMenu(fileName = "NickNameViewConfig", menuName = "Configs/UI/NickName")]
+    public class NickNameViewConfig : ScriptableConfig
+    {
+        [SerializeField] private Color _ownerNameColor = Color.red;
+        [SerializeField] private Color _friendNameColor = Color.green;
+        [SerializeField] private Color _noFriendColor = Color.white;
+
+        public Color OwnerNameColor => _ownerNameColor;
+        public Color FriendNameColor => _friendNameColor;
+        public Color NoFriendColor => _noFriendColor;
+    }
+}
---
+++ Assets/Scripts/UI/Views/NickNameView.cs
@@ -0,0 +1,40 @@
+﻿using AvatarChat.Core.Components;
+using AvatarChat.UI.Configs;
+using Cysharp.Threading.Tasks;
+using Sirenix.OdinInspector;
+using TMPro;
+using UnityEngine;
+using Zenject;
+
+namespace AvatarChat.UI.Views
+{
+    public class NickNameView : MonoBehaviour
+    {
+        [SerializeField, ReadOnly] private TextMeshProUGUI _textComponent;
+        [SerializeField] private Character _character;
+        [Inject] private NickNameViewConfig _config;
+
+        private async void Start()
+        {
+            _textComponent.text = string.Empty;
+            var token = this.GetCancellationTokenOnDestroy();
+            await UniTask.WaitUntil(() => !_character.Owner.IsEmpty, cancellationToken: token);
+            _textComponent.text = _character.Owner.Name.ToString();
+            _textComponent.color = GetColor();
+
+        }
+
+        private Color GetColor ()
+        {
+            return _character.IsOwner ? _config.OwnerNameColor : _config.NoFriendColor;
+        }
+
+        private void OnValidate()
+        {
+            if (!_textComponent)
+            {
+                _textComponent = GetComponent<TextMeshProUGUI>();
+            }
+        }
+    }
+}
